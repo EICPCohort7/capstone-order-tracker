74b801700db479e46148bf18b20445c7510de0ed
John Barber-Ormerod <jack.barber.ormerod@gmail.com>
2022-11-10 16:24:42 -0500

    Cleaned up the home page a little

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/tjx-logo.png">
+    <HelloWorld msg="Welcome to the TJX CSR home page"/>
   </div>
 </template>
 
