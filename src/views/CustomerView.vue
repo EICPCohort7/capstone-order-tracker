15af05f083e3915981e1e0f53db126cdb1c64b3e
John Barber-Ormerod <jack.barber.ormerod@gmail.com>
2022-11-10 15:42:01 -0500

    Minimum Viable Product

--- src/views/CustomerView.vue
+++ src/views/CustomerView.vue
@@ -5,23 +5,8 @@
     <h1>Customer Information</h1>
   </div>
       <div class="form-group">
-    <select class="custom-select" required>
-      <option value="">Open this select menu</option>
-      <option value="1">Customer ID</option>
-      <option value="2">Email</option>
-    </select>
-    <div class="invalid-feedback">Example invalid custom select feedback</div>
 
     <form id="searching">
-      <div class="form-group">
-        <input
-          type="search"
-          class="form-control"
-          id="entercid"
-          placeholder="Customer ID"
-        />
-      
-      </div>
       <div class="form-group">
         <input
           v-model = "emailValue"
@@ -30,104 +15,13 @@
           id="email"
           placeholder="Email"
         />
+        <!--Search button for the email field-->
+        <button type="button" class="btn btn-outline-danger" v-on:click="onGet">Search</button>
+        <button type="button" class="btn btn-outline-danger" v-on:click="getAll">Get All</button>
       </div>
-      <!--Search button for the email field-->
-      <button type="button" class="btn btn-outline-danger" v-on:click="onGet">Search</button>
-      <button type="button" class="btn btn-outline-danger" v-on:click="getAll">Get All</button>
+      
       
     </form>
-
-<!--Modal-->
-<button id="show-modal" @click="showModal = true" class="btn btn-outline-danger">New Customer</button>
-
-<Teleport to="body">
-  <!-- use the modal component, pass in the prop -->
-  <modal :show="showModal" @close="showModal = false">
-    <template #header>
-      <h3>Create a new Customer</h3>
-    </template>
-    <template #body>
-        <input
-          type="text"
-          class="form-control"
-          id="firstName"
-          placeholder="First Name"
-        />
-        <input
-          type="text"
-          class="form-control"
-          id="middleName"
-          placeholder="Middle Initial"
-        />
-        <input
-          type="text"
-          class="form-control"
-          id="lastName"
-          placeholder="Last Name"
-        />
-        <input
-          type="text"
-          class="form-control"
-          id="phone"
-          placeholder="Phone Number"
-        />
-        <input
-          type="email"
-          class="form-control"
-          id="email"
-          placeholder="Email"
-        />
-        <input
-          type="text"
-          class="form-control"
-          id="streetAdd"
-          placeholder="Street Address"
-        />
-        <input
-          type="text"
-          class="form-control"
-          id="aptNum"
-          placeholder="Apartment Number"
-        />
-        <input
-          type="text"
-          class="form-control"
-          id="city"
-          placeholder="City"
-        />
-        <input
-          type="text"
-          class="form-control"
-          id="state"
-          placeholder="State/Province"
-        />
-        <input
-          type="text"
-          class="form-control"
-          id="zip"
-          placeholder="Zip/Postal Code"
-        />
-        <input
-          type="text"
-          class="form-control"
-          id="country"
-          placeholder="Country"
-        />
-        <input
-          type="text"
-          class="form-control"
-          id="notes"
-          placeholder="Customer Notes"
-        />
-    </template>
-    <template #btn>
-        <button
-                class="btn btn-outline-danger"
-              >Submit</button>
-    </template>
-  </modal>
-</Teleport>
-
 <!--Data Table-->
     <table class="table table-striped">
       <thead>
@@ -142,6 +36,7 @@
       </thead>
       <tbody>
         <tr v-for = "customer in customers" :key="customer.id">
+            <!--Change these to proper fields-->
             <td scope="row">{{ customer.id }}</td>
             <td>{{ customer.name }}</td>
             <td>{{ customer.email }}</td>
@@ -154,27 +49,21 @@
       </tbody>
     </table>
   </div>
-  <!--Test Fetch print-->
-  <!--remove when pushing to main repo-->
-  <!-- <div>
-    {{ customers }}
-  </div> -->
 </template>
 
 
 
 <script>
 import axios from "axios";
-import Modal from '../components/Modal.vue'
 export default {
     data() {
         return {
             customers: [],
-            showModal: false
         };
     },
     mounted: function () {
             axios
+                //Change to eic API endpoint
                 .get(`https://jsonplaceholder.typicode.com/users`)
                 .then((response) => {
                     console.log(response);
@@ -190,6 +79,7 @@ export default {
         // The get method called by the function
         onGet() {
             axios
+                //Change to eic API endpoint
                 .get(`https://jsonplaceholder.typicode.com/users?email=${this.emailValue}`)
                 .then((response) => {
                     console.log(response);
@@ -202,6 +92,7 @@ export default {
         },
         getAll(){
             axios
+                //Change to eic API endpoint
                 .get(`https://jsonplaceholder.typicode.com/users`)
                 .then((response) => {
                     console.log(response);
@@ -213,8 +104,5 @@ export default {
                 });
         }
     },
-    components: {
-    Modal
-  },
 };
 </script>
