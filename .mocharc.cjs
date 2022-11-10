7a0fe0bf04b0ee3b233609a258fe9b9fb7208c4c
John Paxton <johnpaxton@gmail.com>
2022-11-10 15:35:52 -0500

    Added files from eicc-cohort07-capstone

---
+++ .mocharc.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  extension: ['mocha.js', 'mocha.cjs', 'mocha.mjs'],
+  spec: '**/test/*.mocha.js',
+  require: './test/load-env.js',
+};
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'url';
+import CopyPlugin from 'copy-webpack-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const webpackConfig = {
+  entry: {
+    index: './front-end/src/index.js',
+    books: './front-end/src/books.js',
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new CopyPlugin({
+      patterns: [{
+        from: path.resolve(__dirname, 'front-end', 'public'),
+        to: path.resolve(__dirname, 'server/public'),
+      }],
+    }),
+  ],
+  mode: 'development',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'server/public'),
+    clean: {
+      keep: '.gitkeep',
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
+
+export default webpackConfig;
