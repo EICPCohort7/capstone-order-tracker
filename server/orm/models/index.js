e9a5c805de9c8ee70b538814cea9c5ec42681b6d
SaraAdra <adra.s@northeastern.edu>
2022-11-11 17:34:59 -0500

    Added association tests (Customer/Order & Order/Product)

--- server/orm/models/index.js
+++ server/orm/models/index.js
@@ -21,16 +21,16 @@ Customer.hasMany(Order, { foreignKey: 'customerId' });
 Order.belongsTo(Customer, { foreignKey: 'customerId' });
 
 // Order/OrderDetails - one to many
-Order.hasMany(OrderDetails, { foreignKey: 'orderId' });
-OrderDetails.belongsTo(Order, { foreignKey: 'orderId' });
+// Order.hasMany(OrderDetails, { foreignKey: 'orderId' });
+// OrderDetails.belongsTo(Order, { foreignKey: 'orderId' });
 
 // OrderStatus/Order
 OrderStatus.hasMany(Order, { foreignKey: 'orderStatusCode' });
 Order.belongsTo(OrderStatus, { foreignKey: 'orderStatusCode' });
 
 // Product/OrderDetails
-Product.hasMany(OrderDetails, { foreignKey: 'productId' });
-OrderDetails.belongsTo(Product, { foreignKey: 'productId' });
+// Product.hasMany(OrderDetails, { foreignKey: 'productId' });
+// OrderDetails.belongsTo(Product, { foreignKey: 'productId' });
 
 // Order/OrderDetails/Product
 Order.belongsToMany(Product, {
