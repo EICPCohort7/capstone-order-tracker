b146c75eb520721d314e6a8b58804ce9956ea5b1
Peetu K <63013313+pkaan@users.noreply.github.com>
2022-11-11 12:38:55 +0000

    load-env.js

---
+++ test/load-env.js
@@ -0,0 +1,21 @@
+import * as dotenv from 'dotenv';
+import chalk from 'chalk';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+let dotEnvResults = dotenv.config(path.join(__dirname, '../.env'));
+
+if (dotEnvResults.error) {
+  if (dotEnvResults.error.code === 'ENOENT') {
+    console.error(
+      chalk.red.bold(
+        '***** ERROR: Please create a .env file with the appropriate information! *****'
+      )
+    );
+  }
+
+  throw dotEnvResults.error;
+}
