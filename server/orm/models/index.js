a7ffefb596d61d094231c04d870974ff3959d729
sar08278_tjxinc <sar08278@tjx.com>
2022-11-11 14:09:00 -0500

    Added Sequelize Association declarations

--- server/orm/models/index.js
+++ server/orm/models/index.js
@@ -4,7 +4,69 @@ import Order from './Order.js';
 import OrderStatus from './OrderStatus.js';
 import Product from './Product.js';
 import OrderDetails from './OrderDetails.js';
-// Sequelize associations go here...
-// ...
+
+// Sequelize Association Declarations
+
+// Version that uses belongsTo
+// Customer/Address - one to many
+Address.hasMany(Customer, { foreignKey: 'billingAddressId' });
+Customer.belongsTo(Address, { foreignKey: 'billingAddressId' });
+
+// Address/Order - one to many
+Address.hasMany(Order, { foreignKey: 'shippingAddressId' });
+Order.belongsTo(Address, { foreignKey: 'shippingAddressId' });
+
+// Customer/Order - one to many
+Customer.hasMany(Order, { foreignKey: 'customerId' });
+Order.belongsTo(Customer, { foreignKey: 'customerId' });
+
+// Order/OrderDetails - one to many
+Order.hasMany(OrderDetails, { foreignKey: 'orderId' });
+OrderDetails.belongsTo(Order, { foreignKey: 'orderId' });
+
+// OrderStatus/Order
+OrderStatus.hasMany(Order, { foreignKey: 'orderStatusCode' });
+Order.belongsTo(OrderStatus, { foreignKey: 'orderStatusCode' });
+
+// Product/OrderDetails
+Product.hasMany(OrderDetails, { foreignKey: 'productId' });
+OrderDetails.belongsTo(Product, { foreignKey: 'productId' });
+
+// Order/OrderDetails/Product
+Order.belongsToMany(Product, {
+  foreignKey: 'orderId',
+  through: OrderDetails,
+});
+Product.belongsToMany(Order, {
+  foreignKey: 'productId',
+  through: OrderDetails,
+});
+
+/*
+// Version that uses hasOne
+// Customer/Address - one to many
+// Address.hasMany(Customer, { foreignKey: 'billingAddressId' });
+// Customer.hasOne(Address, { foreignKey: 'billingAddressId' });
+
+// Address/Order - one to many
+// Address.hasMany(Order, { foreignKey: 'shippingAddressId' });
+// Order.hasOne(Address, { foreignKey: 'shippingAddressId' });
+
+// Customer/Order - one to many
+// Customer.hasMany(Order, { foreignKey: 'customerId' });
+// Order.hasOne(Customer, { foreignKey: 'customerId' });
+
+// Order/OrderDetails - one to many
+// Order.hasMany(OrderDetails, { foreignKey: 'orderId' });
+// OrderDetails.hasOne(Order, { foreignKey: 'orderId' });
+
+// OrderStatus/Order
+// OrderStatus.hasMany(Order, { foreignKey: 'orderStatusCode' });
+// Order.hasOne(OrderStatus, { foreignKey: 'orderStatusCode' });
+
+// Product/OrderDetails
+// Product.hasMany(OrderDetails, { foreignKey: 'productId' });
+// OrderDetails.hasOne(Product, { foreignKey: 'productId' });
+*/
 
 export { Customer, Address, Product, Order, OrderDetails, OrderStatus };
