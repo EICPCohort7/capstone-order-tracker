8718eca40a2555dc2dd55f56a7f779b253dfcf92
Noelle Fajt <noellefajt@gmail.com>
2022-11-15 14:23:27 -0500

    fake data for customers

---
+++ db/fakeData.js
@@ -0,0 +1,18 @@
+import { faker } from '@faker-js/faker';
+
+console.log(faker.date);
+
+// function createRandomUser(): User {
+//   return {
+//     _id: faker.datatype.uuid(),
+//     avatar: faker.image.avatar(),
+//     birthday: faker.date.birthdate(),
+//     email: faker.internet.email(),
+//     firstName: faker.name.firstName(),
+//     lastName: faker.name.lastName(),
+//     sex: faker.name.sexType(),
+//     subscriptionTier: faker.helpers.arrayElement(['free', 'basic', 'business']),
+//   };
+// }
+
+// const user = createRandomUser();
