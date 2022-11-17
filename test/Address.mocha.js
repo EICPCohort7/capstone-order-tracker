c1397458279c3dc524e93c9775fdc39256abbf1b
Deepti Sachi <deepti_sachi@tjx.com>
2022-11-17 10:33:46 -0600

    modified:   test/Address.mocha.js

--- test/Address.mocha.js
+++ test/Address.mocha.js
@@ -24,28 +24,4 @@ describe('Address Model', () => {
     let testAddress = await Address.findByPk(100000, { logging: false });
     expect(testAddress).to.be.null;
   });
-
-  // From models/index.js - Address.hasMany(Customer, { foreignKey: 'billingAddressId' });
-  it('should query Customers (an association)', async () => {
-    let testAddress = await Address.findByPk(1, { logging: false });
-    expect(testAddress.street).to.equal('lincoln street');
-
-    let customersRecord = await testAddress.getCustomers({ logging: false });
-    expect(customersRecord.length).to.equal(1);
-
-    let firstCustomer = customersRecord[0];
-    expect(firstCustomer.customerId).to.equal(1);
-  });
-
-  // From models/index.js - Address.hasMany(Order, { foreignKey: 'shippingAddressId' });
-  it('should query Orders (an association)', async () => {
-    let testAddress = await Address.findByPk(2, { logging: false });
-    expect(testAddress.street).to.equal('1223 lion ave');
-
-    let ordersRecord = await testAddress.getOrders({ logging: false });
-    expect(ordersRecord.length).to.equal(1);
-
-    let firstOrder = ordersRecord[0];
-    expect(firstOrder.orderId).to.equal(2);
-  });
 });
